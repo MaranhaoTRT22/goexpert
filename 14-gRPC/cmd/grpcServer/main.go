41d5b03b489dfd2008dc7b8c7bbfc53222a229ad
Luiz Alberto O Maranhao <laom@trt22.jus.br>
2025-02-18 10:30:32 -0300

    Alterações durante as aulas

--- 14-gRPC/cmd/grpcServer/main.go
+++ 14-gRPC/cmd/grpcServer/main.go
@@ -14,19 +14,26 @@ import (
 )
 
 func main() {
+	// Abre conexão com o banco de dados
 	db, err := sql.Open("sqlite3", "./db.sqlite")
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
+	// Cria banco de dados
 	categoryDb := database.NewCategory(db)
+	// Cria o serviço que manipula o banco de dados
 	categoryService := service.NewCategoryService(*categoryDb)
 
+	// Cria servidor gRPC
 	grpcServer := grpc.NewServer()
+	// Attacha o serviço ao servidor gRPC
 	pb.RegisterCategoryServiceServer(grpcServer, categoryService)
+	// Habilita interação com o Evans
 	reflection.Register(grpcServer)
 
+	// Abre porta TCP pra acessar o serviço gRPC
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		panic(err)
